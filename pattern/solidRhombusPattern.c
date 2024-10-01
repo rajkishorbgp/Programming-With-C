bf8c88c1a5b016f3bdae276c1bc45b03c6107342
Raj Kishor <rajkishor6@gmail.com>
2024-10-01 10:23:36 +0530

    solid rhombus pattern

---
+++ pattern/solidRhombusPattern.c
@@ -0,0 +1,39 @@
+/*
+SOLID RHOMBUS pattern
+       * * * * *
+     * * * * *
+   * * * * *
+  * * * * *
+* * * * *
+
+*/
+
+#include<stdio.h>
+void main()
+{
+    int n=5;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n-i-1; j++)
+        {
+            printf("  ");
+        }
+        for (int k = 0; k < n; k++)
+        {
+            printf("* ");
+        }
+        
+        printf("\n");
+    }
+    
+}
+
+/*
+  output:
+        * * * * * 
+      * * * * *
+    * * * * *
+  * * * * *
+* * * * * 
+
+*/
